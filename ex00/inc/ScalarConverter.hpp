00d87acb2d8244a2a96c620557ff8ae8740c5b1b
mingde <myuen@student.42singapore.sg>
2025-12-26 21:27:05 +0800

    edit typos

--- ex00/inc/ScalarConverter.hpp
+++ ex00/inc/ScalarConverter.hpp
@@ -6,16 +6,26 @@
 /*   By: myuen <myuen@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/11 16:53:29 by myuen             #+#    #+#             */
-/*   Updated: 2025/12/25 21:32:26 by myuen            ###   ########.fr       */
+/*   Updated: 2025/12/26 17:20:41 by myuen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SCALARCONVERTER_HPP
 #define SCALARCONVERTER_HPP
 
-#include <iostream>
+#include <string>
 
+class ScalarConverter
+{
+    ScalarConverter() {};
+    ScalarConverter(const ScalarConverter &);
+    ~ScalarConverter() {};
 
+    ScalarConverter & operator=(const ScalarConverter &);
+    
+public:
+    static void convert(const std::string & input);
+};
 
 
 #endif
--- ex00/src/ScalarConverter.cpp
+++ ex00/src/ScalarConverter.cpp
@@ -6,12 +6,30 @@
 /*   By: myuen <myuen@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/11 16:53:29 by myuen             #+#    #+#             */
-/*   Updated: 2025/12/25 21:33:21 by myuen            ###   ########.fr       */
+/*   Updated: 2025/12/26 17:21:23 by myuen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
+#include <iostream>
 
 using std::string;
 using std::endl;
 using std::cout;
+
+ScalarConverter::ScalarConverter(const ScalarConverter & other)
+{
+    if (this != &other)
+        operator=(other);
+}
+
+ScalarConverter & ScalarConverter::operator=(const ScalarConverter & other)
+{
+    (void) other;
+    return *this;
+}
+
+void ScalarConverter::convert(const std::string & input)
+{
+    
+}
