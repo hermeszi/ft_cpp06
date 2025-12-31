1a497c971f320daba3a2130944c5d5ea9fed2b1e
mingde <myuen@student.42singapore.sg>
2025-12-31 22:18:20 +0800

    edit exercise 00

--- ex00/inc/ScalarConverter.hpp
+++ ex00/inc/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: myuen <myuen@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/11 16:53:29 by myuen             #+#    #+#             */
-/*   Updated: 2025/12/26 17:20:41 by myuen            ###   ########.fr       */
+/*   Updated: 2025/12/31 16:05:24 by myuen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,26 @@
 
 #include <string>
 
+
 class ScalarConverter
 {
+    enum Type_ { 
+        CHAR, 
+        INT, 
+        FLOAT, 
+        DOUBLE, 
+        SPECIAL_FLOAT,  // nanf, +inff, -inff
+        SPECIAL_DOUBLE,  // nan, +inf, -inf
+        INVALID
+    };
+
     ScalarConverter() {};
     ScalarConverter(const ScalarConverter &);
     ~ScalarConverter() {};
 
     ScalarConverter & operator=(const ScalarConverter &);
+    
+    static Type_ detechType(const std::string & input);
     
 public:
     static void convert(const std::string & input);
--- ex00/src/ScalarConverter.cpp
+++ ex00/src/ScalarConverter.cpp
@@ -6,16 +6,19 @@
 /*   By: myuen <myuen@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/11 16:53:29 by myuen             #+#    #+#             */
-/*   Updated: 2025/12/26 17:21:23 by myuen            ###   ########.fr       */
+/*   Updated: 2025/12/31 22:00:37 by myuen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 #include <iostream>
+#include <cstdlib>
 
 using std::string;
 using std::endl;
 using std::cout;
+
+ScalarConverter::ScalarConverter() {};
 
 ScalarConverter::ScalarConverter(const ScalarConverter & other)
 {
@@ -29,7 +32,128 @@ ScalarConverter & ScalarConverter::operator=(const ScalarConverter & other)
     return *this;
 }
 
+static bool hasOneDecimal(const std::string & str)
+{
+    if (str.empty())
+        return false;
+    bool hasDecimal = false;
+    for (size_t i = 0; i < str.length(); ++i)
+    {
+        if (str.at(i) == '.')
+        {
+            if (hasDecimal == false)
+                hasDecimal = true;
+            else
+                return false;
+        }
+    }
+    return hasDecimal;
+}
+
+static bool isValidNum(const std::string & str)
+{
+    if (str.empty())
+        return false;
+        
+    bool hasDecimal = false;
+    bool hasDigit = false;
+
+    for (size_t i = 0; i < str.length(); ++i)
+    {
+        if (str.at(i) == '.')
+        {
+            if (hasDecimal == false)
+                hasDecimal = true;
+            else
+                return false;
+        }
+        else if (str.at(i) < 58 && str.at(i) > 47)
+            hasDigit = true;
+        else
+            return false;      
+    }
+    return hasDigit;
+}
+
+Type_ ScalarConverter::detechType(const std::string & input)
+{
+    if (input.empty())
+        return INVALID;
+        
+    //detech char and special f and d
+    if (input.length() == 3 && input.at(0) == '\'' && input.at(2) == '\'')
+        return CHAR;
+    else if (input == "nanf" || input == "+inff" || input == "-inff")
+        return SPECIAL_FLOAT;
+    else if (input == "nan" || input == "+inf" || input == "-inf")
+        return SPECIAL_DOUBLE;
+
+    //check for Sign and f and .
+    bool hasF = false;
+    bool hasDecimal = false;
+    bool hasSign = false;
+
+    std::string temp(input);
+    if (temp.at(0) == '-' || temp.at(0) == '+')
+    {
+        hasSign = true;
+        temp.erase(0,1);
+    }
+    if (temp.empty())
+        return INVALID;
+    if (temp.at(temp.length()-1) == 'f')
+    {
+        hasF = true;
+        temp.erase(temp.length()-1, 1);
+    }
+    if (temp.empty())
+        return INVALID;
+        
+    hasDecimal = hasOneDecimal(temp);
+    if (isValidNum(temp))
+    {
+        if (hasF && hasDecimal)
+            return FLOAT;
+        else if (hasDecimal)
+            return DOUBLE;
+        else if (!hasF)
+            return INT;
+    }
+    return INVALID;   
+}
+static void printInt(int value)
+{
+    
+}
+
 void ScalarConverter::convert(const std::string & input)
 {
+    Type_ type = detechType(input);
+    
+    if (type == INVALID)
+    {
+        cout << "Invalid Input" << endl;
+        return ;
+    }
+    if (type == INT)
+    {
+        cout << "-- TYPE deteched: INT --" << endl;
+        int value = std::stoi(input); // no range test yet
+    }
+    else if (type == FLOAT)
+    {
+        cout << "-- TYPE deteched: FLOAT --" << endl;
+        float value = std::stof(input); // no range test yet
+    }
+    else if (type == DOUBLE)
+    {
+        cout << "-- TYPE deteched: DOUBLE --" << endl;
+        float value = std::stod(input); // no range test yet
+    }
+    else if (type == CHAR)
+    {
+        cout << "-- TYPE deteched: CHAR --" << endl;
+        char value = input.at(1);
+    }
     
 }
